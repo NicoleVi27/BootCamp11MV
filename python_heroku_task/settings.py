115ff875013c1fb54c11c9d9e622dda65abf8b8e
Milagros Nicole Villalba Espinoza <mvillale@vision.local>
2024-07-26 20:29:18 -0400

    Actualizacion

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'python_heroku_task',#ADD despliegue VERCEL
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'python_heroku_task.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
